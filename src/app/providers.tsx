58519bec1b121d49c76bd3844dbe38fa97089765
tomivm <tomivm98@gmail.com>
2024-08-22 00:29:03 +0200

    Add Fluent UI V9 implementing a providet to useContext

---
+++ src/app/providers.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import {
+  createDOMRenderer,
+  RendererProvider,
+  FluentProvider,
+  webDarkTheme,
+  SSRProvider,
+} from '@fluentui/react-components';
+
+const renderer = createDOMRenderer();
+
+/**
+ * Providers component.
+ *
+ * This component wraps other components with a set of providers
+ * for Fluent UI, SSR, and a custom renderer.
+ *
+ * @param {Object} props - The properties for the Providers component.
+ * @param {React.ReactNode} props.children - The child components to be wrapped by the Providers.
+ * @returns {React.Element} The Providers component with child components.
+ */
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  // If the component has mounted, return a set of providers.
+  return (
+    <RendererProvider renderer={renderer || createDOMRenderer()}>
+      <SSRProvider>
+        <FluentProvider theme={webDarkTheme}>{children}</FluentProvider>
+      </SSRProvider>
+    </RendererProvider>
+  );
+}
