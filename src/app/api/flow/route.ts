0ae8515db6b6b79c6dbfa01af7e85667ab455966
tomivm <tomivm98@gmail.com>
2024-08-23 17:01:06 +0200

    Create endpoint to Post to our AI flow

---
+++ src/app/api/flow/route.ts
@@ -0,0 +1,35 @@
+export async function POST(req: Request) {
+  const body = await req.json();
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestBody = JSON.stringify(body);
+  console.log(requestBody);
+  const requestHeaders = new Headers({ 'Content-Type': 'application/json' });
+
+  const apiKey = process.env.AZURE_FLOW_KEY;
+  if (!apiKey) {
+    throw new Error('A key should be provided to invoke the endpoint');
+  }
+  requestHeaders.append('Authorization', 'Bearer ' + apiKey);
+
+  requestHeaders.append('azureml-model-deployment', 'fluidmind-dnyzk-1');
+
+  const url = 'https://fluidmind-dnyzk.eastus2.inference.ml.azure.com/score';
+  const response = await fetch(url, {
+    method: 'POST',
+    body: requestBody,
+    headers: requestHeaders,
+  });
+  try {
+    if (response.ok) {
+      const data = await response.json();
+      return Response.json(data);
+    } else {
+      throw new Error('Request failed with status code' + response.status);
+    }
+  } catch (error) {
+    console.error(error);
+    return new Response('Error generating response', { status: 500 });
+  }
+}
