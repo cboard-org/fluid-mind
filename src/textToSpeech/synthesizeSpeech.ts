ed4d6310102fe39797a63804fc774b8ba1548f70
tomivm <tomivm98@gmail.com>
2024-09-02 12:32:21 +0200

    Add TTS logic

---
+++ src/textToSpeech/synthesizeSpeech.ts
@@ -0,0 +1,43 @@
+import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
+import { SpeechSynthesizer } from 'microsoft-cognitiveservices-speech-sdk';
+
+type AzureSynthesizerType = sdk.SpeechSynthesizer | null;
+
+let azureSynthesizer: AzureSynthesizerType = null;
+
+const initAzureSynthesizer = () => {
+  azureSynthesizer = null;
+  if (!process.env.NEXT_PUBLIC_AZURE_SPEECH_KEY || !process.env.NEXT_PUBLIC_AZURE_SPEECH_REGION)
+    return;
+  const speechConfig = sdk.SpeechConfig.fromSubscription(
+    process.env.NEXT_PUBLIC_AZURE_SPEECH_KEY,
+    process.env.NEXT_PUBLIC_AZURE_SPEECH_REGION,
+  );
+  const audioConfig = sdk.AudioConfig.fromDefaultSpeakerOutput();
+  azureSynthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+  azureSynthesizer.properties.setProperty(
+    'SpeechServiceConnection_SynthVoice',
+    'en-GB-SoniaNeural',
+  );
+};
+
+const speak = (text: string) => {
+  if (azureSynthesizer)
+    azureSynthesizer.speakTextAsync(
+      text,
+      (result) => {
+        if (result) {
+          azureSynthesizer?.close();
+          initAzureSynthesizer();
+        }
+      },
+      (error) => {
+        console.log(error);
+        azureSynthesizer?.close();
+        initAzureSynthesizer();
+      },
+    );
+};
+initAzureSynthesizer();
+
+export { speak };
