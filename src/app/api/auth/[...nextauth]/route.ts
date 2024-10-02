62a03efc1ed824d5b8bf82c59ceb0d061babd63c
tomivm <tomivm98@gmail.com>
2024-10-02 19:02:10 +0200

    Move auth config to lib folder

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,56 +1,6 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { compare } from 'bcryptjs';
-import dbConnect from '@/src/lib/dbConnect';
-import User from '@/src/dataBase/models/User';
+import authConfig from '@/src/lib/next-auth/config';
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        try {
-          if (!credentials) return null;
-          if (!credentials.email || !credentials.password) {
-            return null;
-          }
-          await dbConnect();
-
-          const { email, password } = credentials;
-
-          const user = await User.findOne({ email });
-          if (!user) {
-            return null;
-          }
-          console.log('user', user);
-          console.log('password', password);
-          const isMatch = await compare(password, user.password);
-          if (!isMatch) {
-            return null;
-          }
-
-          return {
-            id: user._id,
-            email: user.email,
-            username: user.username,
-          };
-        } catch (error) {
-          console.error(error);
-          return null;
-        }
-      },
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/login',
-  },
-});
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST };
---
+++ src/lib/next-auth/config.ts
@@ -0,0 +1,57 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcryptjs';
+import dbConnect from '@/src/lib/dbConnect';
+import User from '@/src/dataBase/models/User';
+import { AuthOptions } from 'next-auth';
+
+const config = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+          if (!credentials) return null;
+          if (!credentials.email || !credentials.password) {
+            return null;
+          }
+          await dbConnect();
+
+          const { email, password } = credentials;
+
+          const user = await User.findOne({ email });
+          if (!user) {
+            return null;
+          }
+          console.log('user', user);
+          console.log('password', password);
+          const isMatch = await compare(password, user.password);
+          if (!isMatch) {
+            return null;
+          }
+
+          return {
+            id: user._id,
+            email: user.email,
+            username: user.username,
+          };
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+    error: '/login', // Error code passed in query string as ?error=
+  },
+} satisfies AuthOptions;
+
+export default config;
